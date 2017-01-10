a718280a1c2edc8608a90a9c62a9fb9f4a318308
jskorups <skorupski.jakub@gmail.com>
2017-01-10 19:59:36 +0100

    addin data object adding enter key event adding localstorage function

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-/**
- * Created by Kuba on 1/3/2017.
- */
+var data = (localStorage.getItem('todoList')) ? JSON.parse(localStorage.getItem('todoList')):{
+    todo:[],
+    completed:[]
+};
+
+
+JSON.parse(localStorage.getItem(("todoList")));
 
 //Remove and complete icons in SVG format:
 
@@ -8,24 +12,76 @@ var removeSVG = '<svg version="1.1"  xmlns="http://www.w3.org/2000/svg" xmlns:xl
 var completeSVG = '<svg version="1.1"  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect y="0" class="noFill" width="22" height="22"/><g><path class="fill" d="M9.7,14.4L9.7,14.4c-0.2,0-0.4-0.1-0.5-0.2l-2.7-2.7c-0.3-0.3-0.3-0.8,0-1.1s0.8-0.3,1.1,0l2.1,2.1l4.8-4.8c0.3-0.3,0.8-0.3,1.1,0s0.3,0.8,0,1.1l-5.3,5.3C10.1,14.3,9.9,14.4,9.7,14.4z"/></g></svg>'
 
 
+renderTodoList();
 
 //Add input value if there is text
 
 document.getElementById('add').addEventListener('click' , function() {
     var value = document.getElementById('item').value;
     if (value) {
-        addItemTodo(value);
+
+        addItemToDOM(value);
         document.getElementById('item').value = '';
+        data.todo.push(value);
+        dataObjectUpdated();
+
     }
 });
+
+    document.getElementById('item').addEventListener("keydown", function(e) {
+
+        var value = this.value;
+        if (e.code === 'Enter' && value) {
+            addItem(value);
+
+        }
+
+});
+
+function addItem (value) {
+
+    addItemToDOM(value);
+    document.getElementById('item').value = '';
+    data.todo.push(value);
+    dataObjectUpdated();
+}
+
+
+function renderTodoList(){
+    if(data.todo.length && ! data.completed.length) return;
+
+for (var i = 0; i < data.todo.length; i++) {
+    var value = data.todo[i];
+    addItemToDOM(value);
+}
+
+for (var j = 0; j < data.completed.length; j++) {
+    var value = data.completed[j];
+    addItemToDOM(value, true);
+
+    }
+}
+
+function dataObjectUpdated() {
+    localStorage.setItem('todoList', JSON.stringify(data));
+}
+
 
 function removeItem () {
 
     var item = this.parentNode.parentNode;
     var parent = item.parentNode;
+    var id = parent.id;
+    var value = item.innerText;
 
+    if (id === 'todo') {
+        data.todo.splice(data.todo.indexOf(value), 1);
+    } else {
+        data.completed.splice(data.todo.indexOf(value), 1);
+    }
+
+    dataObjectUpdated();
     parent.removeChild(item);
-
 }
 
 function completeItem() {
@@ -33,6 +89,18 @@ function completeItem() {
     var item = this.parentNode.parentNode;
     var parent = item.parentNode;
     var id = parent.id;
+    var value = item.innerText;
+
+    if (id === 'todo') {
+
+        data.todo.splice(data.todo.indexOf(value), 1);
+        data.completed.push(value);
+    } else {
+
+        data.completed.splice(data.todo.indexOf(value), 1);
+        data.todo.push(value);
+    }
+    dataObjectUpdated();
 
     var target =  (id === 'todo') ? document.getElementById('completed'):document.getElementById('todo');
 
@@ -42,10 +110,9 @@ function completeItem() {
 }
 
 //add a new item to the
-function addItemTodo(text) {
+function addItemToDOM(text,completed) {
 
-    var list = document.getElementById('todo');
-    console.log(list);
+    var list = (completed) ? document.getElementById('completed'):document.getElementById('todo');
 
     var item = document.createElement('li');
     item.innerText = text;
